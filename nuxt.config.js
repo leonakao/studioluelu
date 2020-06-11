91114d930c40972a2a5980b6ae5d2e3846d855b1
Leonardo Nakao <=>
2020-06-11 00:42:00 -0300

    Remove dark theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from "vuetify/es5/util/colors";
-
 export default {
     mode: "universal",
     server: {
@@ -87,15 +85,6 @@ export default {
             themes: {
                 light: {
                     primary: "#A70E0D"
-                },
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
                 }
             }
         }
